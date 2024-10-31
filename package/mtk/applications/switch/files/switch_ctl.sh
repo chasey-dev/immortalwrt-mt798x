3f61d8c0f078195dbacf4ffb8eee09230e40c600
ellenyoung0912@gmail.com <ellenyoung0912@gmail.com>
2024-10-31 17:21:29 +0800

    try to add the switch chip an8855 support for ax3000t

--- package/mtk/applications/switch/files/switch_ctl.sh
+++ package/mtk/applications/switch/files/switch_ctl.sh
@@ -2,49 +2,67 @@
 
 [ ! -x /usr/sbin/switch ] && return
 
-# power off ports
-# $1 ports list, ex: "1 2 3"
-sw_poweroff_ports() {
-	local ori_value
-	local set_value
-	[ -z "$1" ] && return 1
-	for p in $1; do
-		# read original value of register 0
-		ori_value=$(switch phy cl22 r $p 0 | awk -F'=' '{print $3}')
+# determine switch type
+get_switch_type() {
+    if [ -e /proc/air_sw/device ] && grep -q "an8855" /proc/air_sw/device; then
+        echo "an8855"
+    else
+        echo "mt753x"
+    fi
+}
 
-		# register 0, bit 11 is power down control bit, set to 1
-		set_value=$(($ori_value | 0x800))
-		switch phy cl22 w $p 0 $set_value >/dev/null 2>&1
-	done
+SWITCH_TYPE=$(get_switch_type)
+
+# set register address via switch type
+if [ "$SWITCH_TYPE" = "an8855" ]; then
+    REG_ADDR=1
+else
+    REG_ADDR=0
+fi
+
+# shutdown ports
+# $1 port lists，eg："1 2 3"
+sw_poweroff_ports() {
+    local ori_value
+    local set_value
+    [ -z "$1" ] && return 1
+    for p in $1; do
+        # read original value of register
+        ori_value=$(switch phy cl22 r $p $REG_ADDR | awk -F'=' '{print $3}')
+
+        # set power down control (bit 11) to 1
+        set_value=$(($ori_value | 0x800))
+        switch phy cl22 w $p $REG_ADDR $set_value >/dev/null 2>&1
+    done
 }
 
 # power on ports
-# $1 ports list, ex: "1 2 3"
+# $1 port lists，eg："1 2 3"
 sw_poweron_ports() {
-	local ori_value
-	local set_value
-	[ -z "$1" ] && return 1
-	for p in $1; do
-		# read original value of register 0
-		ori_value=$(switch phy cl22 r $p 0 | awk -F'=' '{print $3}')
+    local ori_value
+    local set_value
+    [ -z "$1" ] && return 1
+    for p in $1; do
+        # read original value of register
+        ori_value=$(switch phy cl22 r $p $REG_ADDR | awk -F'=' '{print $3}')
 
-		# register 0, bit 11 is power down control bit, set to 0
-		set_value=$(($ori_value & ~0x800))
-		switch phy cl22 w $p 0 $set_value >/dev/null 2>&1
-	done
+        # clear power down control (bit 11)
+        set_value=$(($ori_value & ~0x800))
+        switch phy cl22 w $p $REG_ADDR $set_value >/dev/null 2>&1
+    done
 }
-# restart Auto-neg on ports
-# $1 ports list, ex: "1 2 3"
-sw_restart_port() {
-	local ori_value
-	local set_value
-	[ -z "$1" ] && return 1
-	for p in $1; do
-		# read original value of register 0
-		ori_value=$(switch phy cl22 r $p 0 | awk -F'=' '{print $3}')
 
-		# register 0, bit 9 is Restart Auto-Negotiation bit, set to 1
-		set_value=$(($ori_value | 0x200))
-		switch phy cl22 w $p 0 $set_value >/dev/null 2>&1
-	done
+# auto negotiate port restart
+sw_restart_port() {
+    local ori_value
+    local set_value
+    [ -z "$1" ] && return 1
+    for p in $1; do
+
+        ori_value=$(switch phy cl22 r $p $REG_ADDR | awk -F'=' '{print $3}')
+
+        # set auto negotiate bit (bit 9) to 1
+        set_value=$(($ori_value | 0x200))
+        switch phy cl22 w $p $REG_ADDR $set_value >/dev/null 2>&1
+    done
 }
